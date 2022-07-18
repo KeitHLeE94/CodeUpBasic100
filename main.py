6a7217f9786b6b153d16259db53a34ce8a752a66
KeitHLeE94 <wogus4187@naver.com>
2022-07-19 00:53:03 +0900

    Basic 5/100 Problems Solved

---
+++ main.py
@@ -0,0 +1,9 @@
+from prob_6001 import prob_6001
+from prob_6002 import prob_6002
+from prob_6003 import prob_6003
+from prob_6004 import prob_6004
+from prob_6005 import prob_6005
+
+
+if __name__ == '__main__':
+    prob_6005()
